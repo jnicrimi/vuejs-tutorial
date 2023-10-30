8f5b5f9d5fef6e0144fd1d758ad6e01420468c20
Junichiro Imai <jnicrimi@gmail.com>
2023-10-30 21:53:19 +0900

    feat: vue.js tutorial

---
+++ app/src/components/TutorialStep11.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ChildComp from './ChildComp11.vue'
+</script>
+
+<template>
+  <ChildComp />
+</template>
---
+++ app/src/components/TutorialStep12.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+import ChildComp from './ChildComp12.vue'
+
+const greeting = ref('Hello from parent')
+</script>
+
+<template>
+  <ChildComp :msg="greeting" />
+</template>
